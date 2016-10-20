954f850c51879a16ec40ff5d0f3d4d05eb293d4e
Evilkookey <evilkookey64@gmail.com>
2016-10-20 21:09:03 +0100

    these are the scripts

---
+++ CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "TimerThing") 
+		{
+			col.SendMessage ("Slow");
+		}
+			
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ timerThingController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timerThingController : MonoBehaviour 
+{
+
+	public float forwardSpeed;
+	float verticalSpeed;
+	int slow = 1;
+
+	CharacterController characterController;
+
+	// Use this for initialization
+	void Start () 
+	{
+		characterController = GetComponent<CharacterController> ();
+	}
+
+	void Slow()
+	{
+		slow *= -1;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (characterController.isGrounded) 
+		{
+
+			verticalSpeed = 0;
+			slow = 1;
+		}
+		else
+		{
+			verticalSpeed += slow * Physics.gravity.y * Time.deltaTime * 0.4f;
+			if (verticalSpeed > -1.0f) 
+			{
+				verticalSpeed = -1.0f;
+			}
+		}
+
+		//Debug.Log (slow + "   " + verticalSpeed + "");
+
+		Vector3 speed = new Vector3 (-forwardSpeed, verticalSpeed, 0f);
+
+		characterController.Move (speed * Time.deltaTime);
+	}
+}
